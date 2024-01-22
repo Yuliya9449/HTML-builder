fc90e78a0e7da7166198a81db6719f239dfde488
Yuliya Kotova <yulya9449@yandex.ru>
2024-01-22 21:16:24 +0300

    refactor: solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,43 +3,36 @@ const path = require('path');
 const { mkdir: mkdirPromise } = require('fs/promises');
 const { readdir: readdirPromise } = require('fs/promises');
 const { copyFile: copyFilePromise } = require('fs/promises');
-const { unlink: unlinkPromise } = require('fs/promises');
+const { rm: rmPromise } = require('fs/promises');
 
 const pathToFilesDir = path.resolve(__dirname, 'files');
 const pathToCopyFilesDir = path.resolve(__dirname, 'files-copy');
 
-async function copyDir() {
+async function copyDirectory(pathFromElem, pathToCopyElem) {
   try {
-    // Создание папки files-copy, флаг { recursive: true } означает,
-    // что если папка существует, то её не нужно создавать
-    await mkdirPromise(pathToCopyFilesDir, { recursive: true });
+    await rmPromise(pathToCopyElem, { recursive: true, force: true });
 
-    // Чтение содержимого папки files-copy
-    const filesToRemove = await readdirPromise(pathToCopyFilesDir, {
+    await mkdirPromise(pathToCopyElem);
+
+    const elementsInDir = await readdirPromise(pathFromElem, {
       withFileTypes: true,
     });
 
-    // Очистка папки files-copy
-    filesToRemove.forEach(async (file) => {
-      const pathToFile = path.resolve(file.path, file.name);
-      await unlinkPromise(pathToFile);
-    });
+    elementsInDir.forEach(async (elem) => {
+      const pathToInnerElem = path.resolve(elem.path, elem.name);
+      const pathToCopyInnerElem = path.resolve(pathToCopyElem, elem.name);
 
-    // Чтение содержимого папки files
-    const fileNamesArr = await readdirPromise(pathToFilesDir);
+      if (elem.isDirectory()) {
+        await copyDirectory(pathToInnerElem, pathToCopyInnerElem);
+      }
 
-    // Копирование файлов
-    fileNamesArr.forEach(async (fileName) => {
-      const pathToFile = path.resolve(pathToFilesDir, fileName);
-      const pathToCopyFile = path.resolve(pathToCopyFilesDir, fileName);
-
-      await copyFilePromise(pathToFile, pathToCopyFile);
+      if (elem.isFile()) {
+        await copyFilePromise(pathToInnerElem, pathToCopyInnerElem);
+      }
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-copyDir();
-
-// node 04-copy-directory/
+copyDirectory(pathToFilesDir, pathToCopyFilesDir);
