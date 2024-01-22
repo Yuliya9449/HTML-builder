8d69ba0a40d57b33397523aed64ad7520562df27
Yuliya Kotova <yulya9449@yandex.ru>
2024-01-22 20:49:16 +0300

    feat: add solution  02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const readline = require('readline');
+const fs = require('fs');
+const { stdin, stdout } = process;
+
+const pathToFile = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(pathToFile);
+const readlineInterface = readline.createInterface(stdin, pathToFile);
+
+stdout.write('\nPlease, enter some text\n\n');
+
+readlineInterface.on('line', readlineHandler);
+
+async function readlineHandler(answer) {
+  try {
+    if (answer.trim() === 'exit') {
+      sayGoodBye();
+    } else {
+      await fs.promises.appendFile(pathToFile, `${answer}\n`);
+    }
+  } catch (error) {
+    console.log(`${error.name}: ${error.message}`);
+  }
+}
+
+process.on('SIGINT', sayGoodBye);
+
+function sayGoodBye() {
+  stdout.write('\nGood bye!!!\n');
+  writeStream.end();
+  process.exit();
+}
+
+// node 02-write-file/
